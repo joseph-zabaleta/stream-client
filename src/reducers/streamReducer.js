058c2ca14cef643f67c50dc779cd1b3c60b71828
joseph-zabaleta <joseph.l.zabaleta@gmail.com>
2021-01-22 12:51:07 -0800

    Base project Complete for reference

---
+++ src/reducers/streamReducer.js
@@ -0,0 +1,28 @@
+import _ from 'lodash';
+
+import {
+	CREATE_STREAM,
+	FETCH_STREAM,
+	FETCH_STREAMS,
+	DELETE_STREAM,
+	EDIT_STREAM,
+} from '../actions/types';
+
+const streamReducer = (state = {}, action) => {
+	switch (action.type) {
+		case CREATE_STREAM:
+		case FETCH_STREAM:
+		case EDIT_STREAM:
+			return { ...state, [action.payload.id]: action.payload };
+
+		case FETCH_STREAMS:
+			return { ...state, ..._.mapKeys(action.payload, 'id') };
+
+		case DELETE_STREAM:
+			return _.omit(state, action.payload);
+		default:
+			return state;
+	}
+};
+
+export default streamReducer;
